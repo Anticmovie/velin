4faffe9da9845299799e03e6b5cb9f5255ea6b0e
Anticmovie <anticmovie@gmail.com>
2026-07-20 10:05:49 +0200

    auto-deploy: dashboard 2026-07-20 10:05

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 ﻿window.DASHBOARD_DATA = {
-    "generated":  "2026-07-20 10:01",
+    "generated":  "2026-07-20 10:05",
     "summary":  {
                     "total":  8,
                     "local":  8,
